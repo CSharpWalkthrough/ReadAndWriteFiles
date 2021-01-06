5fc656b40b9b5b9c2852a3e70969d7f661a9422c
sshimabuku <shimasstrafez@gmail.com>
2021-01-05 23:14:41 -0500

    Refactoring

--- ReadAndWriteFiles.Tutorial/Program.cs
+++ ReadAndWriteFiles.Tutorial/Program.cs
@@ -7,36 +7,90 @@ using System.Threading.Tasks;
 
 namespace ReadAndWriteFiles.Tutorial
 {
+    /// <summary>
+    /// A program to demonstrate the use of System.IO methods to read and 
+    /// write to files. Use of breakpoints and debugging is suggested.
+    /// </summary>
     class Program
     {
+        private const string READ_FILE_PATH = "read.txt";
+        private const string WRITE_FILE_PATH = "write.txt";
         static void Main(string[] args)
         {
-            ReadFromFile("read.txt");
+            Console.WriteLine("---READ FILE---" + Environment.NewLine);
+            ReadFromFile(READ_FILE_PATH);
             WriteToFile();
-            AppendToFile("write.txt");
 
-            Console.WriteLine();
+            Console.WriteLine("---WRITE TO FILE---" + Environment.NewLine);
+            Console.WriteLine("Check the write.txt file and any key to continue");
+            Console.ReadKey();
 
-            ReadFromFile("write.txt");
-            Console.ReadLine();
+            AppendToFile(WRITE_FILE_PATH);
+            Console.WriteLine("Check the write.txt file again");
+            Console.ReadKey();
         }
 
+        #region Read File
         private static void ReadFromFile(string path)
         {
             // Check if file exists
             if (!File.Exists(path))
                 throw new FileNotFoundException();
 
+            // Using OpenText method
+            OpenText(path);
+
+            // Using ReadAllText method
+            ReadAllText(path);
+
+            // Using ReadAllLines method
+            ReadAllLines(path);
+        }
+
+        private static void OpenText(string path)
+        {
             // Initialize StreamReader with OpenText
+            Console.WriteLine("Using OpenText :" + Environment.NewLine);
             using (StreamReader sr = File.OpenText(path))
             {
                 string line;
                 while ((line = sr.ReadLine()) != null) // Assign new line to variable
                     Console.WriteLine(line); // Print read line
             }
-
         }
 
+        private static void ReadAllText(string path)
+        {
+            Console.WriteLine(Environment.NewLine + "Using ReadAllText :" + Environment.NewLine);
+
+            // The following will return all the file text into a string
+            string content = File.ReadAllText(path);
+            Console.WriteLine(content);
+
+            // We could also split our content into an array and loop through it
+            Console.WriteLine(Environment.NewLine + "Looping through lines" + Environment.NewLine);
+
+            string[] newLine = new[] { Environment.NewLine };
+            string[] contentLines = content.Split(newLine, StringSplitOptions.None);
+            foreach (string line in contentLines)
+                Console.WriteLine(line);
+        }
+
+        private static void ReadAllLines(string path)
+        {
+            Console.WriteLine(Environment.NewLine + "Using ReadAllLines :" + Environment.NewLine);
+
+            // The following will return an array of string where each element represents a line of content
+            string[] lines = File.ReadAllLines(path);
+            foreach (string line in lines)
+                Console.WriteLine(line);
+
+            Console.WriteLine();
+        }
+        #endregion
+
+
+        #region Write to File
         private static void WriteToFile()
         {
             string path = "write.txt";
@@ -49,6 +103,7 @@ namespace ReadAndWriteFiles.Tutorial
                 sw.WriteLine("Third line");
             }
         }
+
         private static void AppendToFile(string path)
         {
             // Check if file exists
@@ -61,7 +116,7 @@ namespace ReadAndWriteFiles.Tutorial
 
             // Append text to end of file using AppendText
             // Initialize StreamWriter with AppendTextw
-            using (StreamWriter sw = File.AppendText(path)) 
+            using (StreamWriter sw = File.AppendText(path))
             {
                 // Write content using StreamWriter object
                 sw.WriteLine("Using AppendText method");
@@ -69,5 +124,6 @@ namespace ReadAndWriteFiles.Tutorial
             }
 
         }
+        #endregion
     }
 }
